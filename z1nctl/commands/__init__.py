f6b4b23503d7daf9675c1f54a7e2d1e19890423e
andrew-ai <adoing@zirrusone.com>
2022-07-18 08:27:06 -0500

    Add sql queries

--- z1nctl/commands/__init__.py
+++ z1nctl/commands/__init__.py
@@ -1,26 +1,63 @@
 import click
 import frappe
+from psycopg2 import sql
+
 
 @click.command('get-perm')
 @click.argument('doctype')
-@click.argument('fieldtype', required=False)
-def get_perm(doctype, fieldtype):
+@click.argument('fieldType', required=False)
+def get_perm(doctype, fieldType):
     # SQL query to pull the doc type
+    if doctype and fieldType:
+        print(f"Executing get-perm sql query with parameters: doctype:{doctype}, fieldType:{fieldType}")
+        frappe.db.sql('''
+        SELECT fieldname, label, permlevel
+        FROM tabDocField
+        WHERE tdf.parent={doctype} AND tdf.fieldtype={fieldType}
+        ''').format(
+            doctype = sql.Identifier(doctype),
+            fieldType = sql.Identifier(fieldType)
+        )
+        print("Sucessfully executed get-perm sql query")
+        return
     if doctype:
-        print(f"Not implemented, should query DB for permissions from doctype: {doctype}")
-        # return frappe.db.sql("")
+        print(f"Executing get-perm sql query with parameters: doctype:{doctype}")
+        frappe.db.sql('''
+        SELECT fieldname, label, permlevel
+        FROM tabDocField
+        WHERE tdf.parent={doctype}
+        ''').format(
+            doctype = sql.Identifier(doctype)
+        )
+        print("Sucessfully executed get-perm sql query")
 
-    if doctype and fieldtype:
-        print(f"Not implemented, doctype: {doctype}, fieldtype:{fieldtype}")
 
 @click.command('set-perm')
 @click.argument('doctype')
-@click.argument('fieldtype')
-def set_perm(doctype, fieldtype):
-    if doctype:
-        print(f"Not implemented, should query DB for permissions from doctype: {doctype}")
-    if doctype and fieldtype:
-        print(f"Not implemented, doctype: {doctype}, fieldtype:{fieldtype}")
+@click.argument('fieldType')
+@click.argument('permLevel')
+def set_perm(doctype, fieldType, permLevel):
+    # Validate inputs
+    try:
+        permLevel = int(permLevel)
+        if permLevel < 0 or permLevel > 9:
+            raise Exception
+    except:
+        print("permLevel must be an integer between 0-9")
+        return
+
+    if doctype and fieldType and permLevel:
+        print(f"Executing set-perm sql query with parameters: doctype:{doctype}, fieldType:{fieldType}, permLevel:{permLevel}")
+        frappe.db.sql('''
+        UPDATE tabDocField AS tdf
+        SET permlevel = {permLevel}
+        WHERE tdf.parent={doctype} AND tdf.fieldtype={fieldType}
+        ''').format(
+            permLevel = sql.Literal(permLevel),
+            doctype = sql.Identifier(doctype),
+            fieldType = sql.Identifier(fieldType)
+        )
+        print("Sucessfully executed set-perm sql query")
 
 commands = [
     get_perm, set_perm
